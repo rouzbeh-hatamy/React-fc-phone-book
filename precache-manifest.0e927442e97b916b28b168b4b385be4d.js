36d413a2485a2dfdda90941af925801acc24cbf8
Rouzbeh Hatamy <roozbeh.hatamy@gmail.com>
2020-07-23 11:18:51 +0430

    Updates

---
+++ precache-manifest.0e927442e97b916b28b168b4b385be4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1762d687cd28246415ecf70c02acb58",
+    "url": "/React-fc-phone-book/index.html"
+  },
+  {
+    "revision": "62c600ad27b3e51bceb3",
+    "url": "/React-fc-phone-book/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "e5bbd2df3b5a949c3383",
+    "url": "/React-fc-phone-book/static/css/main.e8d8bb39.chunk.css"
+  },
+  {
+    "revision": "62c600ad27b3e51bceb3",
+    "url": "/React-fc-phone-book/static/js/2.55cfac1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-fc-phone-book/static/js/2.55cfac1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5bbd2df3b5a949c3383",
+    "url": "/React-fc-phone-book/static/js/main.5c21f956.chunk.js"
+  },
+  {
+    "revision": "a839c7a7def4168e61e2",
+    "url": "/React-fc-phone-book/static/js/runtime-main.08aaf698.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React-fc-phone-book/precache-manifest.0e927442e97b916b28b168b4b385be4d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-fc-phone-book/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
